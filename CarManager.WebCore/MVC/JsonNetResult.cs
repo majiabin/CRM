c073de430ebe437de2a6c71aa2b18ec13c91a3ba
马佳斌 <121845422@qq.com>
2016-09-13 23:33:16 +0800

    添加jsonNetResult

---
+++ CarManager.WebCore/MVC/JsonNetResult.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace CarManager.WebCore.MVC
+{
+    public class JsonNetResult : JsonResult
+    {
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            else
+            {
+                var response = context.HttpContext.Response;
+                response.ContentType = !string.IsNullOrEmpty(ContentType) ? ContentType : "application/json";
+                if (ContentEncoding != null)
+                {
+                    response.ContentEncoding = ContentEncoding;
+                }
+                var jsonSerializerSettings = new JsonSerializerSettings();
+                jsonSerializerSettings.ContractResolver=new CamelCasePropertyNamesContractResolver();//契约
+                jsonSerializerSettings.DateFormatString = "yyyy-MM-dd HH:mm:ss";
+                string json = JsonConvert.SerializeObject(Data, Formatting.None, jsonSerializerSettings);
+                response.Write(json);
+            }
+
+            base.ExecuteResult(context);
+        }
+
+    }
+}
